4dcc4ffb6f2213318f2707340f9b0aa5a788da5d
hamhead1005 <abrahamham360@gmail.com>
2021-04-11 16:09:21 -0700

    Added the Intro Screen.
    Our logo and team name will display for 3 seconds then move on to the actual game portion.

--- app/src/main/java/com/example/cs2450androidapp/MainActivity.java
+++ app/src/main/java/com/example/cs2450androidapp/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.cs2450androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * Count Down 3 seconds then change over to the game.
+         */
+        new CountDownTimer(3000,1000)
+        {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(MainActivity.this, ConcentrationGame.class);
+                startActivity(intent);
+            }
+        }.start();
     }
 }
