eea0d340d9f9e9b6f36e1ab422a27f498eee45b7
Abrahan <abrahamham360@gmail.com>
2021-04-24 16:52:50 -0700

    Made Changes to Structure

--- app/src/main/java/com/example/cs2450androidapp/MainMenu.java
+++ app/src/main/java/com/example/cs2450androidapp/MainMenu.java
@@ -20,6 +20,7 @@ public class MainMenu extends AppCompatActivity {
     public void onActivityResult(int requestCode,int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+
         if(requestCode == 1)
         {
             if(resultCode == RESULT_OK)
