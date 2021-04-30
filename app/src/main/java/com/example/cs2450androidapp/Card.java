de8faf7260548c67d9e34a99147afe509a2c3364
hamhead1005 <abrahamham360@gmail.com>
2021-04-29 20:24:49 -0700

    Created a way to flip the cards
    Check if the answers are correct:
    Alert user to click checkAnswer button when 2 cards have been flipped over.
    Upon Checking Answer if Cards match allow user to continue clicking, If they dont match flip the 2 cards over.
    
    Alert User if they won when all cards have been verified.
    
    Filp all the cards over on try-again

--- app/src/main/java/com/example/cs2450androidapp/Card.java
+++ app/src/main/java/com/example/cs2450androidapp/Card.java
@@ -5,10 +5,16 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Card extends androidx.appcompat.widget.AppCompatTextView {
-    String assignedWord;
-    boolean clickedState = false;
+    private static int cardWidth = 180;
+    private static int cardHeight = 260;
+    private String assignedWord;
+    private boolean clickedState = false;
+    private boolean beenChecked = false; //Used to mark correct tiles
 
     public Card(Context context) {
         super(context);
@@ -18,25 +24,56 @@ public class Card extends androidx.appcompat.widget.AppCompatTextView {
         this.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                clickedState = true;
-                setText(assignedWord);
-                setBackgroundColor(Color.WHITE);
+                if(ConcentrationGame.getClickCount() == 2){
+                    Toast toast = Toast.makeText(getContext(), "Click the Check Answers button first!", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else {
+                    clickedState = true;
+                    setText(assignedWord);
+                    setBackgroundColor(Color.WHITE);
+                    ConcentrationGame.addClick();
+                }
             }
         });
     }//End Click Event Listener
 
     public void setProperties() {
         setBackgroundColor(Color.YELLOW);
-        setWidth(200);
-        setHeight(280);
-        setTextSize(15);
+        setWidth(cardWidth);
+        setHeight(cardHeight);
+        setTextSize(13);
         setGravity(Gravity.CENTER);
         Typeface b = Typeface.defaultFromStyle(Typeface.BOLD);
         setTypeface(b);
         setOnClickListener();
     }//end setProperties
 
+    public void flip(){
+        clickedState = false;
+        beenChecked = false;
+        setText("");
+        setBackgroundColor(Color.YELLOW);
+    }//end flip method
+
     public void setAssignedWord(String word) {
         assignedWord = word;
     }//end setAssignedWord
+
+    //Button has been clicked but not checked
+    public boolean getClicked() {
+        return clickedState;
+    }
+
+    public boolean beenChecked() {
+        return beenChecked;
+    }
+
+    public void setBeenChecked(){
+        this.beenChecked = true;
+    }
+
+    public String getWord() {
+        return assignedWord;
+    }
 }
--- app/src/main/java/com/example/cs2450androidapp/ConcentrationGame.java
+++ app/src/main/java/com/example/cs2450androidapp/ConcentrationGame.java
@@ -8,15 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.GridLayout;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class ConcentrationGame extends AppCompatActivity {
+    private static int buttonsClicked = 0;
     private int buttonCount = 0;
     GridLayout simpleGrid;
     String [] finalWords;
+    public Card [] board;
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        buttonsClicked = 0; //reset button clicked counter
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,20 +71,108 @@ public class ConcentrationGame extends AppCompatActivity {
         String gameSize = extras.getString("GAME_SIZE");
         buttonCount = Integer.parseInt(gameSize);
         finalWords = getFinalWords(buttonCount);
+        board = new Card[buttonCount]; //Intiallize size of the Card array
 
         //Create Board
         simpleGrid = (GridLayout) findViewById(R.id.CardGrid);
-
         for(int i = 0; i < buttonCount; i++){
             Card c = new Card(this);
             c.setId(i);
             c.setAssignedWord(finalWords[i]);
             c.setProperties();
             simpleGrid.addView(c);
+            board[i] = c;
         }//end for
 
+
+        //Try Again Button
+        Button tryButton = findViewById(R.id.TryAgainButton);
+        tryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonsClicked = 0;
+
+                flipAllCards();
+            }
+        });
+
+
+        //Check Answers Button
+        Button checkButton = findViewById(R.id.CheckAnswerButton);
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonsClicked = 0;
+
+                for(int i = 0; i < buttonCount; i++) {
+                    if(board[i].getClicked()){
+                        if(!matchFound(board[i])){
+                            flipAllCards();
+                        }
+                    }
+                }//end for
+
+                if(gameWon()){
+                    Toast toast = Toast.makeText(ConcentrationGame.this, "You Won!", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
     }//end OnCreate
 
+
+    /*                              *
+       Methods outside of On Create *
+                                    *
+     */
+
+    /**
+     *  Checks to see if the player matched all the cards
+     *
+     * @return true if game won. Triggers message
+     */
+    private boolean gameWon() {
+        boolean result = false;
+        int correctAnswers = 0;
+
+        for(int i = 0; i < buttonCount; i++){
+            if(board[i].beenChecked()){
+                correctAnswers++;
+            }
+        }
+
+        if(correctAnswers == buttonCount){
+            return true;
+        }
+
+        return result;
+    }//end gameWon
+
+    /**
+     * Used to flip all Cards
+     */
+    private void flipAllCards() {
+        for(int i = 0; i < buttonCount; i++){
+            board[i].flip();
+        }
+    }//end flipAllCards
+
+    private void filpIncorrectCards(){
+        for(int i = 0; i < buttonCount; i++){
+            if(!board[i].beenChecked()){
+                board[i].flip();
+            }
+        }
+    }
+
+    /**
+     * Create the random string used for the card initilization
+     *
+     * Makes sure each word occurs twice and is in random order
+     *
+     * @param buttons number of buttons needed
+     * @return Array of Strings
+     */
     public String [] getFinalWords(int buttons)
     {
         String [] output = new String[buttons];
@@ -98,4 +195,43 @@ public class ConcentrationGame extends AppCompatActivity {
 
         return output;
     }//end getFinalWords
+
+    /**
+     *  Used to check if a match is found between passed in target and other clicked cards
+     *
+     *  Also makes sure target Card is not checked against itself
+     *
+     * @param target card you want to compare
+     * @return true if matchfound. false if not
+     */
+    public boolean matchFound(Card target){
+        boolean found = false;
+
+        for(int x = 0; x < buttonCount; x++) {
+            if (board[x].getClicked() && target.getWord().equals(board[x].getWord()) && target.getId() != board[x].getId()) {
+                target.setBeenChecked();
+                board[x].setBeenChecked();
+                return true;
+            }
+        }//end for
+
+        return found;
+    }//end matchFound()
+
+    /**
+     * add 1 to the clickCounter
+     */
+    public static void addClick(){
+        buttonsClicked++;
+    }//end addClick()
+
+
+    /**
+     * Getter for static click counter
+     *
+     * @return number of clicks
+     */
+    public static int getClickCount() {
+        return  buttonsClicked;
+    }//end getClickCount()
 }//End Class
